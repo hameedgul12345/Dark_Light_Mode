a84bb3eae3c073059796f015f35e4efe3fc5eaf7
Hameed Gul <hamidkhan31590@gmail.com>
2024-09-15 10:31:24 +0500

    Dark and Light Mode

---
+++ app.js
@@ -0,0 +1,35 @@
+var body = document.getElementById("body");
+var btn = document.getElementById("btn");
+
+var btnBackgroundColor = "black";
+
+var btnColor = "white";
+
+var bodyBackgroundColor = "white";
+
+btn.style.backgroundColor = btnBackgroundColor;
+btn.style.color = btnColor;
+
+btn.style.padding = "14px 15px";
+btn.style.borderRadius = "14px";
+
+function btnDarkLightMode() {
+  console.log("Hello");
+  //   body.classList.add("background");
+  body.style.backgroundColor = bodyBackgroundColor 
+  if(bodyBackgroundColor=)
+    console.log(bodyBackgroundColor);
+    
+//   btn.style.backgroundColor = btnBackgroundColor = "black" ? "white" : "black";
+btn.style.backgroundColor = btnBackgroundColor = "black" ? "white" : "black";
+
+
+    btn.style.color = btnColor = ("white"
+      ? "black"
+      : "white")
+}
+// var btn=document.getElementById.addEventListener('click',function btnDarkLightMode(){
+//     var body=document.getElementById('body')
+//     var btn=document.getElementById('btn')
+//     body.classList.add('background');
+// })
