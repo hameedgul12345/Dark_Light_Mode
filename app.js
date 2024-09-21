84b239a3d570be8cd0625f552b0ec9d8b39d88af
Hameed Gul <hamidkhan31590@gmail.com>
2024-09-21 09:35:36 +0500

    Center dev

--- app.js
+++ app.js
@@ -15,34 +15,32 @@ btn.style.borderRadius = "14px";
 
 function btnDarkLightMode() {
   console.log("Hello");
-    body.classList.add("background");
-  if(bodyBackgroundColor=='white'){
-     bodyBackgroundColor='black'
-    body.style.backgroundColor = bodyBackgroundColor
-    btnColor='black'
-    btn.style.color=btnColor 
-    btnBackgroundColor='white'
-    btn.style.backgroundColor=btnBackgroundColor
-    btn.innerHTML='Day'
+  body.classList.add("background");
+  if (bodyBackgroundColor == "white") {
+    bodyBackgroundColor = "black";
+    body.style.backgroundColor = bodyBackgroundColor;
+    btnColor = "black";
+    btn.style.color = btnColor;
+    btnBackgroundColor = "white";
+    btn.style.backgroundColor = btnBackgroundColor;
+    btn.innerHTML = "Day";
+  } else {
+    bodyBackgroundColor = "white";
+    body.style.backgroundColor = bodyBackgroundColor;
+    btnColor = "white";
+    btn.style.color = btnColor;
+    btnBackgroundColor = "black";
+    btn.style.backgroundColor = btnBackgroundColor;
+    btn.innerHTML = "Night";
   }
-  else{
-    bodyBackgroundColor='white'
-    body.style.backgroundColor = bodyBackgroundColor 
-    btnColor='white'
-    btn.style.color=btnColor
-    btnBackgroundColor="black"
-    btn.style.backgroundColor=btnBackgroundColor
-     btn.innerHTML='Night'
-  }
-    console.log(bodyBackgroundColor);
-    
-//   btn.style.backgroundColor = btnBackgroundColor = "black" ? "white" : "black";
-// btn.style.backgroundColor = btnBackgroundColor = "black" ? "white" : "black";
+  console.log(bodyBackgroundColor);
 
+  //   btn.style.backgroundColor = btnBackgroundColor = "black" ? "white" : "black";
+  // btn.style.backgroundColor = btnBackgroundColor = "black" ? "white" : "black";
 
-    // btn.style.color = btnColor = ("white"
-    //   ? "black"
-    //   : "white")
+  // btn.style.color = btnColor = ("white"
+  //   ? "black"
+  //   : "white")
 }
 // var btn=document.getElementById.addEventListener('click',function btnDarkLightMode(){
 //     var body=document.getElementById('body')
